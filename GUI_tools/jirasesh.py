347eab9887a47a79ab9820bf21fb5811be9a667b
pcalebho <pcalebho@gmail.com>
2023-04-15 17:45:01 -0500

    log_hours in decent spot

--- GUI_tools/jirasesh.py
+++ GUI_tools/jirasesh.py
@@ -26,11 +26,11 @@ class JiraInst():
             for s in self._states:
                 state_in_JQL = '\"' + s + '\"'
                 query_JQL = 'assignee = \"' + assignee + '\" and sprint in (openSprints(),futureSprints()) and status = ' + state_in_JQL
-                issue_list[s] = self.jira_session.search_issues(query_JQL)
+                issue_list[s] = self.jira_session.search_issues(query_JQL, fields = 'summary')
         else:
             state_in_JQL = '\"' + search_state + '\"'
             query_JQL = 'assignee = \"' + assignee + '\" and sprint in (openSprints(),futureSprints()) and status = ' + state_in_JQL
-            issue_list = self.jira_session.search_issues(query_JQL)
+            issue_list = self.jira_session.search_issues(query_JQL, fields = 'summary')
 
         return issue_list
     
@@ -53,89 +53,93 @@ class JiraInst():
             
     def  log_hours(self, hours, user, issue, started = None):
         hours_JQL = hours + "h"    
-        if started is None:
-            self.jira_session.add_worklog(issue.key, timeSpent = hours_JQL)
-        else:
-            self.jira_session.add_worklog(issue.key, timeSpent = hours_JQL, started = started)            #add 0 hours a week later
+        try:
+            if started is None:
+                self.jira_session.add_worklog(issue.key, timeSpent = hours_JQL)
+            else:
+                self.jira_session.add_worklog(issue.key, timeSpent = hours_JQL, started = started)
+        except:
+            pass
 
-    # def upload(self, closed_sprints, prompt, duplicates, fatal_errors, verbose, pageview,
-    #         dry_run, prepend_sprint, config):
-    #     """Use TOKEN to connect to Jira site and upload issues from CONFIG."""
-    #     # parse yaml into list of story, task, bugs
-    #     config = yaml.load(config, Loader=yaml.Loader)
-    #     issues = []
+    def upload(self, closed_sprints, prompt, duplicates, fatal_errors, verbose, pageview,
+            dry_run, prepend_sprint, issues_yml):
+        """Use TOKEN to connect to Jira site and upload issues from CONFIG."""
+        # parse yaml into list of story, task, bugs
+        config = yaml.load(issues_yml, Loader=yaml.Loader)
+        issues = []
 
-    #     try:
-    #         for issue_cfg in config['issues']:
-    #             issue = deepcopy(config['defaults'])
-    #             issue.update(issue_cfg)
-    #             issues.append(issue)
+        try:
+            for issue_cfg in config['issues']:
+                issue = deepcopy(config['defaults'])
+                issue.update(issue_cfg)
+                issues.append(issue)
 
-    #     except KeyError as err:
-    #         click.secho("Config is missing key '{}'".format(err.args[0]), err=True)
+        except KeyError as err:
+            click.secho("Config is missing key '{}'".format(err.args[0]), err=True)
 
-    #     # validate issues
-    #     v = Validator(jira, fatal_errors=fatal_errors, verbose=verbose,
-    #                 duplicates=duplicates, closed_sprints=closed_sprints,
-    #                 prepend_sprint=prepend_sprint)
-    #     vissues = v.validate(issues)
+        # validate issues
+        v = Validator(jira, fatal_errors=fatal_errors, verbose=verbose,
+                    duplicates=duplicates, closed_sprints=closed_sprints,
+                    prepend_sprint=prepend_sprint)
+        vissues = v.validate(issues)
 
-    #     if len(vissues) == 0:
-    #         click.secho("No issues to upload after validation.", err=True, fg='red')
-    #         return
+        if len(vissues) == 0:
+            click.secho("No issues to upload after validation.", err=True, fg='red')
+            return
 
-    #     # Handle various options
-    #     result = ("Validated {} issues in projects {}. "
-    #             .format(len(vissues), set(vi['project'] for vi in vissues)))
-    #     if pageview:
-    #         click.echo_via_pager(result + "\n\n"
-    #             + pprint.pformat({issue['summary']: issue for issue in vissues}))
-    #     if dry_run or (prompt and not click.confirm("\n" + result + "Proceed?")):
-    #         return
+        # Handle various options
+        result = ("Validated {} issues in projects {}. "
+                .format(len(vissues), set(vi['project'] for vi in vissues)))
+        if pageview:
+            click.echo_via_pager(result + "\n\n"
+                + pprint.pformat({issue['summary']: issue for issue in vissues}))
+        if dry_run or (prompt and not click.confirm("\n" + result + "Proceed?")):
+            return
 
-    #     results = []
-    #     with click.progressbar(
-    #         vissues,
-    #         label='Creating issues via the Jira API...',
-    #         # item_show_func=lambda i: i['summary'],
-    #     ) as bar:
-    #         for issue in bar:
-    #             sprint_id = issue.pop('sprint').id
-    #             epic_id = issue.pop('epic').id
-    #             board = issue.pop('board')
+        results = []
+        with click.progressbar(
+            vissues,
+            label='Creating issues via the Jira API...',
+            # item_show_func=lambda i: i['summary'],
+        ) as bar:
+            for issue in bar:
+                sprint_id = issue.pop('sprint').id
+                epic_id = issue.pop('epic').id
+                board = issue.pop('board')
 
-    #             res = jira.create_issue(issue)
-    #             results.append(res)
-    #             jira.add_issues_to_sprint(sprint_id=sprint_id,
-    #                                     issue_keys=[res.key])
+                res = jira.create_issue(issue)
+                results.append(res)
+                jira.add_issues_to_sprint(sprint_id=sprint_id,
+                                        issue_keys=[res.key])
 
-    #             # the add_issues_to_epic API appears to be deprecated
-    #             try:
-    #                 jira.add_issues_to_epic(epic_id=epic_id,
-    #                                         issue_keys=[res.key])
-    #             except NotImplementedError:
-    #                 res.update(fields={'parent': {'id': epic_id}})
+                # the add_issues_to_epic API appears to be deprecated
+                try:
+                    jira.add_issues_to_epic(epic_id=epic_id,
+                                            issue_keys=[res.key])
+                except NotImplementedError:
+                    res.update(fields={'parent': {'id': epic_id}})
 
-    #             jira.add_worklog(res.key, timeSpent="0h")
-    #             #jira.transitions(res)
+                jira.add_worklog(res.key, timeSpent="0h")
+                #jira.transitions(res)
 
                 
-    #     for res in results:
-    #         click.secho("Created issue {!r}".format(res))
+        for res in results:
+            click.secho("Created issue {!r}".format(res))
 
 
-    #     # if failed, prompt to launch editor
-    #     # run sprint selector
-    #     # if dry-run or verbose, print final issues
-    #     # if not dry-run upload with jira authenticator
-    #     return 0
+        # if failed, prompt to launch editor
+        # run sprint selector
+        # if dry-run or verbose, print final issues
+        # if not dry-run upload with jira authenticator
+        return 0
 
 if __name__ == "__main__":
     a = JiraInst()
     print(a._states)
     issue = a.get_issues('Caleb Ho','To Do')
     # issue = a.get_issues('Caleb Ho','Blocked')
-    print(issue)
-    #To Do -> Blocked works but not vice versa for change_state
+    b = a.jira_session.issue(id = 'ZTRASH-1746')
+    # print(a.jira_session.fields())
+    print(b.key + ' ' + b.get_field('summary'))
     # print(a.change_state(assignee = 'Caleb Ho', final_state = 'To Do', issues_2_change = issue))
-    print(a.change_state(assignee = 'Caleb Ho', final_state = 'Blocked', issues_2_change = issue))
+    # print(a.change_state(assignee = 'Caleb Ho', final_state = 'Blocked', issues_2_change = issue))
