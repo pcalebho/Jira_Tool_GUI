8b82ac76cf438d9d19443f701a44c5f59ffc044d
pcalebho <pcalebho@gmail.com>
2023-04-16 07:37:41 -0500

    deleted verbose code

--- GUI_tools/validator.py
+++ GUI_tools/validator.py
@@ -14,20 +14,17 @@ class ValidationError(Exception):
 class Validator(object):
     issuetypes = ('Story', 'Task')
 
-    def __init__(self, jira, fatal_errors=True, verbose=True, duplicates=False,
+    def __init__(self, jira, fatal_errors=True, duplicates=False,
                  closed_sprints=False, prepend_sprint=False):
         self.jira = jira
         self.fatal_errors = fatal_errors
         self.duplicates = duplicates
         self.closed_sprints = closed_sprints
         self.prepend_sprint = prepend_sprint
-        self.verbose = verbose
         self.initialize()
 
     def initialize(self):
         self.projects = {proj.key: proj for proj in self.jira.projects()}
-        if self.verbose:
-            click.secho("Found projects: {}".format(pprint.pformat(self.projects)), fg='yellow')
 
         self.project_components = {}
         self.boards = {}
@@ -35,9 +32,6 @@ class Validator(object):
     def validate_name_to_single_user(self, name):
         if name == 'self':
             user = self.jira.user(self.jira.current_user())
-            if self.verbose:
-                click.secho("Using current user (via token) '{!r}'".format(user),
-                            fg='yellow')
         else:
             users = self.jira.search_users(query=name)
 
@@ -46,9 +40,6 @@ class Validator(object):
             elif len(users) > 1:
                 raise ValidationError("Multiple users found for '{}': {}"
                                       .format(name, users))
-            elif self.verbose:
-                click.secho("Found user {!r} for query '{}'".format(users[0], name),
-                            fg='yellow')
             user = users[0]
 
         return {'id': user.accountId}
@@ -62,16 +53,9 @@ class Validator(object):
     def validate_board_name_to_board(self, board_name):
         if board_name not in self.boards:
             self.boards.update({board.name: board for board in self.jira.boards()})
-            if self.verbose:
-                click.secho("Found boards: {}".format(pprint.pformat(self.jira.boards())),
-                            fg='yellow')
             if board_name not in self.boards:
                 raise ValidationError("board name '{}' not found"
                                       .format(board_name))
-            if self.verbose:
-                click.secho("Found board {!r} for name '{}'"
-                            .format(self.boards[board_name], board_name),
-                            fg='yellow')
 
         return self.boards[board_name]
 
@@ -81,10 +65,6 @@ class Validator(object):
 
         if self.closed_sprints:
             sprints += self.jira.sprints(board.id, state='closed', startAt=50)  # for testing with closed sprints
-
-        if self.verbose:
-            click.secho("Found sprints: {}".format(pprint.pformat(sprints)),
-                        fg='yellow')
 
         filtered = [s for s in sprints if s.state == sprint_desc]
         if len(filtered) == 0:
@@ -102,11 +82,6 @@ class Validator(object):
         project_key = self.validate_project_key(project_key)
         if project_key not in self.project_components:
             self.project_components[project_key] = self.jira.project_components(project_key)
-            if self.verbose:
-                click.secho("Project '{}' components are {}"
-                            .format(project_key,
-                                    pprint.pformat(self.project_components[project_key])),
-                            fg='yellow')
 
         output = []
         for component in components:
@@ -155,8 +130,6 @@ class Validator(object):
                         raise ValidationError("Illegal sprint name."
                                               " Cannot prepend to issue summaries.")
 
-                if self.verbose:
-                    click.secho("\nValidated issue {}".format(pprint.pformat(issue)), fg='green')
                 issues.append(issue)
             except KeyError as err:
                 click.secho("issue dict is missing key '{}'".format(err.args[0]),
